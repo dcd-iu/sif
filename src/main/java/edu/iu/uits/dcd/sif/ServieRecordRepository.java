4abd3ee762df95914de3735e9efb2619fee2a856
asikprad <asikprad@iu.edu>
2017-09-27 12:28:40 -0400

    feat(sif): initial code for sif app

---
+++ src/main/java/edu/iu/uits/dcd/sif/ServieRecordRepository.java
@@ -0,0 +1,6 @@
+package edu.iu.uits.dcd.sif;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ServieRecordRepository extends CrudRepository<ServiceRecord, Long> {
+}
---
+++ src/test/java/edu/iu/uits/dcd/sif/SifApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.iu.uits.dcd.sif;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SifApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
